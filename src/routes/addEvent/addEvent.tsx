df13e77e156ac24ad36cec1fc3fdce1b2d0aea33
mjagi <m.jagiello03@gmail.com>
2024-05-31 19:34:16 +0200

    Comit od maksa

--- src/routes/addEvent/addEvent.tsx
+++ src/routes/addEvent/addEvent.tsx
@@ -3,13 +3,20 @@ import InputBoxAddEvent from "./components/inputAddEvent.tsx";
 import './addEvent.css';
 import BigInputAddEvent from "./components/bigInputAddEvent.tsx";
 import {useNavigate} from "react-router-dom";
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import dayjs, {Dayjs} from 'dayjs';
+import {DateTimePicker} from "@mui/x-date-pickers";
+
 
 function AddEventPage() {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [image, setImage] = useState('');
+    const [startDate, setStartDate] = useState(dayjs());
+    const [endDate, setEndDate] = useState(dayjs());
     const navigate = useNavigate();
+
 
     return (
         <div className="container-event">
@@ -17,24 +24,46 @@ function AddEventPage() {
                 <div className="first-row-event">
                     <h1>Dodaj Ofertę/Event</h1>
                     <div className="buttons-save-discard">
-                        <button className="save-button" onClick={() => {navigate("/home")}}>Zapisz</button>
-                        <button className="discard-button" onClick={() => {navigate("/home")}}>Odrzuć</button>
+                        <button className="save-button" onClick={() => {
+                            navigate("/home")
+                        }}>Zapisz
+                        </button>
+                        <button className="discard-button" onClick={() => {
+                            navigate("/home")
+                        }}>Odrzuć
+                        </button>
                     </div>
                 </div>
                 <form className="input-boxes">
-                    <div className="left-input-boxes">
-                        <InputBoxAddEvent value={name} onChange={(event) => setName(event.currentTarget.value)} label="Nazwa"/>
-                        <BigInputAddEvent value={description} onChange={(event) => setDescription(event.currentTarget.value)}
-                                  label="Opis usług"/>
-                        <InputBoxAddEvent value={price} onChange={(event) => setPrice(event.currentTarget.value)} label="Cena"/>
-                        <InputBoxAddEvent value={image} onChange={(event) => setImage(event.currentTarget.value)} label="Dodaj zdjęcie"/>
+                    <InputBoxAddEvent value={name} onChange={(event) => setName(event.currentTarget.value)}
+                                      label="Nazwa"/>
+                    <BigInputAddEvent value={description}
+                                      onChange={(event) => setDescription(event.currentTarget.value)}
+                                      label="Opis usług"/>
 
+                    <div className="date-pickers">
+                        <DateTimePicker
+                            label="Data rozpoczęcia"
+                            value={startDate}
+                            onChange={(newDate) => setStartDate(newDate as Dayjs)}
+                            format="DD/MM/YYYY HH:MM"
+                            ampm={false}
+                        />
+
+                        <DateTimePicker
+                            label="Data zakończenia"
+                            value={endDate}
+                            onChange={(newDate) => setEndDate(newDate as Dayjs)}
+                            ampm={false}
+                            format="DD/MM/YYYY HH:MM"
+                        />
                     </div>
-                    <div className="right-input-boxes">
-                        <div className="calendar">
-                            <img src="src/routes/addEvent/calendarp.png" alt="calendar"/>
-                        </div>
-                    </div>
+
+                    <InputBoxAddEvent value={price} onChange={(event) => setPrice(event.currentTarget.value)}
+                                      label="Cena"/>
+                    <InputBoxAddEvent value={image} onChange={(event) => setImage(event.currentTarget.value)}
+                                      label="Dodaj zdjęcie"/>
+
                 </form>
             </div>
         </div>
